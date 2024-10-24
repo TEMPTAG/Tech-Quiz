e17eddc7809fbba36c9a90e3086ac14ef4328d08
Ian Ferguson <iansterlingferguson@gmail.com>
2024-10-24 16:04:02 -0700

    component test of start quiz and initial mock question functional.

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,11 +1,32 @@
-// import React from "react";
+import React from "react";
 import Quiz from "../../client/src/components/Quiz";
-// import { mockState } from "../support/utils/helpers";
-// import "@testing-library/cypress/add-commands";
+// import { mount } from "@cypress/react18";
+import mockQuestion from "../fixtures/questions.json";
+import "@testing-library/cypress/add-commands";
 
-describe("Quiz Component Test", () => {
-  it("should render the quiz component", () => {
+describe("<Quiz />", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "/api/questions/random", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: mockQuestion,
+      });
+    }).as("getRandomQuestion");
+  });
+
+  it("should render the quiz page, including the 'Start Quiz' button", () => {
     cy.mount(<Quiz />);
-    cy.get("button").contains("Start Quiz");
+    cy.get(`[data-cy=start-quiz]`).should("contain", "Start Quiz");
+  });
+
+  it("should render the mockQuestion when 'Start Quiz' button is clicked, and verify the H2-Question, numbered selection buttons, and provided answers text body", () => {
+    cy.mount(<Quiz />);
+    cy.get(`[data-cy=start-quiz]`).contains("Start Quiz").click();
+    cy.wait("@getRandomQuestion");
+    cy.get(`[data-cy=question]`).should("contain", mockQuestion[0].question);
+    mockQuestion[0].answers.forEach((answer, index) => {
+      cy.get(`[data-cy=answer-btn-${index}]`).should("contain", index + 1);
+      cy.get(`[data-cy=answer-text-${index}]`).should("contain", answer.text);
+    });
   });
 });
--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,4 +1,12 @@
 import "./commands.ts";
 import { mount } from "cypress/react18";
 
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount;
+    }
+  }
+}
+
 Cypress.Commands.add("mount", mount);
